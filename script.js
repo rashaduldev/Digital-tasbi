10e179d6a7cb3a83d50b9fb684e8cada19c28ad0
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-13 14:40:28 +0600

    id include

---
+++ script.js
@@ -0,0 +1,16 @@
+const subhanallahdisplay=document.getElementById('subhanallahdisplay');
+const subahallahincrement=document.getElementById('subahallahincrement');
+const subahallahdecrement=document.getElementById('subahallahdecrement');
+
+const alhamdullilhdisplay=document.getElementById('alhamdullilhdisplay');
+const Alhamdulillahincrement=document.getElementById('Alhamdulillahincrement');
+const Alhamdulillahdecriment=document.getElementById('Alhamdulillahdecriment');
+
+const allahuakberdisplay=document.getElementById('allahuakberdisplay');
+const allahuakberincrement=document.getElementById('allahuakberincrement');
+const allahuakberdecriment=document.getElementById('allahuakberdecriment');
+
+// Reset button
+const resetbutton=document.getElementById('resetbutton');
+
+
