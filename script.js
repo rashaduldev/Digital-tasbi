79b64a3a3131eee2b0d5096b710f2a6092d57ffe
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-13 14:52:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+// Subhan Allah
 const subhanallahdisplay=document.getElementById('subhanallahdisplay');
 const subahallahincrement=document.getElementById('subahallahincrement');
 const subahallahdecrement=document.getElementById('subahallahdecrement');
 
+
+// Alhamdulillah
 const alhamdullilhdisplay=document.getElementById('alhamdullilhdisplay');
 const Alhamdulillahincrement=document.getElementById('Alhamdulillahincrement');
 const Alhamdulillahdecriment=document.getElementById('Alhamdulillahdecriment');
 
+// AllahHuAkber
 const allahuakberdisplay=document.getElementById('allahuakberdisplay');
 const allahuakberincrement=document.getElementById('allahuakberincrement');
 const allahuakberdecriment=document.getElementById('allahuakberdecriment');
@@ -14,3 +18,24 @@ const allahuakberdecriment=document.getElementById('allahuakberdecriment');
 const resetbutton=document.getElementById('resetbutton');
 
 
+let subhanallahdisplayp=0;
+let alhamdullilhdisplayp=0;
+let allahuakberdisplayp=0;
+
+
+
+subahallahincrement.addEventListener('click',function nam() {
+    if (subhanallahdisplayp==33) {
+        return alert('Please filup another');
+    }
+    subhanallahdisplayp+=1;
+    subhanallahdisplay.innerText=subhanallahdisplayp;
+})
+// dicrement button
+subahallahdecrement.addEventListener('click',function nam() {
+    if (subhanallahdisplayp==0) {
+        return alert('Please increment this');
+    }
+    subhanallahdisplayp-=1;
+    subhanallahdisplay.innerText=subhanallahdisplayp;
+})
