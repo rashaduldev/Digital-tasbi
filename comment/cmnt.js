ea647ad3486a9763e963ad7169fdbb23ea14df35
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-13 16:40:36 +0600

    add

---
+++ comment/cmnt.js
@@ -0,0 +1,8 @@
+const btnclick=document.getElementById('btnclick');
+const textcontainer=document.getElementById('textcontainer');
+const commentText=document.getElementById('commentText');
+
+btnclick.addEventListener('click',function comment() {
+    console.log('button is clicked');
+    
+})
