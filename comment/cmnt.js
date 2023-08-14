5246f0179f2b78bf275e738f26f824ec9fa0ed0c
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-14 20:42:02 +0600

    add

--- comment/cmnt.js
+++ comment/cmnt.js
@@ -3,6 +3,13 @@ const textcontainer=document.getElementById('textcontainer');
 const commentText=document.getElementById('commentText');
 
 btnclick.addEventListener('click',function comment() {
-    console.log('button is clicked');
-    const 
+    // console.log('button is clicked');
+    const newC=commentText.value;
+   
+    console.log(newC);
+    // commentText='';
+    // step3
+    const p=document.createElement('p');
+    p.innerText=newC;
+    textcontainer.appendChild(p);
 })
